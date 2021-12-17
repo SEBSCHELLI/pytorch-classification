1a947518297fd0c611811f0842c2db6b06befa11
SEBSCHELLI <schellhammersebastian@gmail.com>
2021-12-17 11:16:36 +0100

    introduce mixture_of_experts.py for olhs dataset

--- utils/eval.py
+++ utils/eval.py
@@ -13,6 +13,7 @@ def accuracy(output, target, topk=(1,)):
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
+    print(correct.shape)
 
     res = []
     for k in topk:

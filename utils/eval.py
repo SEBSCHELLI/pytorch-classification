2be476bd5ef9125679b2e1b877c6bd7e20aba860
SEBSCHELLI <schellhammersebastian@gmail.com>
2021-12-17 11:26:58 +0100

    introduce mixture_of_experts.py for olhs dataset

--- utils/eval.py
+++ utils/eval.py
@@ -7,27 +7,12 @@ def accuracy(output, target, topk=(1,)):
     maxk = max(topk)
     batch_size = target.size(0)
 
-    print(output.shape)
-    print(target.shape)
-
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
-    print(correct.shape)
-
-    """import torch
-    a = torch.Tensor([[1,2,3,4],
-                      [2,3,2,3],
-                      [2,3,2,3],
-                      [2,3,2,3],
-                      [2,3,2,3]])
-
-    print(a.shape)"""
 
     res = []
     for k in topk:
-        print(k)
-        print(correct[:k].shape)
         correct_k = correct[:k].reshape(-1).float().sum(0)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
